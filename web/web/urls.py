77dcde5b9104b37bb8eb759e449b505eed4c20b9
SEPM26 <sebastian456741@gamil.com>
2023-12-05 19:31:50 -0400

    first commit

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for web project.
+URL configuration for CoderWeb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import *
-from django.conf import settings
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ("pruebatemplate/", Template),
+    path('CoderApp/', include('CoderApp.urls')),
 ]
+
