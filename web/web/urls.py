c4e792a5b73dfb6a40f79c761135b55e9bb476e8
SEPM26 <sebastian456741@gamil.com>
2023-12-05 19:23:55 -0400

    first commit

--- web/web/urls.py
+++ web/web/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
