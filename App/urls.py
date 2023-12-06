3cf904e12e93662c43f8e9fe68592d1cc28b9492
SEPM26 <sebastian456741@gamil.com>
2023-12-05 20:19:24 -0400

    first commit

--- App/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('fecha-actual/', views.mostrar_fecha_actual, name='mostrar_fecha_actual'),
-    path('productos/', views.producto_list, name='producto_list'),
-    path('index/', views.index, name='index'),
-]
