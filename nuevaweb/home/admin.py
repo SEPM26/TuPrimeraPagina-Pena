d38560e2def55f8640a70e9e6c0ce2d4f1bab8fc
SEPM26 <sebastian456741@gamil.com>
2023-12-06 00:46:20 -0400

    home

--- nuevaweb/home/admin.py
+++ nuevaweb/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from  .models import Producto, Libro, PiezaCarro
 
-# Register your models here.
+admin.site.register(Producto)
+admin.site.register(Libro)
+admin.site.register(PiezaCarro)
+
+
