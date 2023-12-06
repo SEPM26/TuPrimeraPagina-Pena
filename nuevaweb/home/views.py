455669a6a3f0004e470f21e7386b1560455f1685
SEPM26 <sebastian456741@gamil.com>
2023-12-05 22:32:23 -0400

    first commit

--- nuevaweb/home/views.py
+++ nuevaweb/home/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
+from .models import Producto
+# Create your views here.
 
-def homeView(request):
-    return render(request, "home/index.html")
+def mostrar_fecha_actual(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request, 'fecha_actual.html', {'fecha_actual': fecha_actual})
 
+def producto_list(request):
+    productos = Producto.objects.all()
+    print(productos)
+    return render(request, 'producto_list.html', {'productos': productos})
+
+def index(request):
+    return render(request, 'index.html')
 
