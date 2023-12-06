799f2ff825494c0b1d719e315907ca9b8063ffc8
SEPM26 <sebastian456741@gamil.com>
2023-12-05 20:47:24 -0400

    first commit

---
+++ nuevaweb/App/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('fecha-actual/', views.mostrar_fecha_actual, name='mostrar_fecha_actual'),
+    path('productos/', views.producto_list, name='producto_list'),
+    path('index/', views.index, name='index'),
+]
